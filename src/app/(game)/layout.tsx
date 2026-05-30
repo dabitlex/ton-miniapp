c026790a67f3b49f20c35f5f1c28c132edc9b242
dabitlex <dabitlex@gmail.com>
2026-05-30 15:38:37 +0200

    Update layout.tsx

--- src/app/(game)/layout.tsx
+++ src/app/(game)/layout.tsx
@@ -1,25 +1,28 @@
 // src/app/(game)/layout.tsx
-import { AuthProvider } from '@/components/providers/AuthProvider'
-import { GameHeader }   from '@/components/layout/GameHeader'
-import { EnergyStrip }  from '@/components/layout/EnergyStrip'
-import { MobileNav }    from '@/components/layout/MobileNav'
-import { EnergyTicker } from '@/components/layout/EnergyTicker'
+import { AuthProvider }   from '@/components/providers/AuthProvider'
+import { TonProvider }    from '@/components/providers/TonProvider'
+import { GameHeader }     from '@/components/layout/GameHeader'
+import { EnergyStrip }    from '@/components/layout/EnergyStrip'
+import { MobileNav }      from '@/components/layout/MobileNav'
+import { EnergyTicker }   from '@/components/layout/EnergyTicker'
 
 export default function GameLayout({ children }: { children: React.ReactNode }) {
   return (
     <AuthProvider>
-      <EnergyTicker />
-      <div className="flex flex-col h-dvh bg-[#0c0c0f] overflow-hidden">
-        <GameHeader />
-        <EnergyStrip />
-        <main
-          className="flex-1 overflow-y-auto overscroll-contain
-                     [scrollbar-width:none] [-webkit-overflow-scrolling:touch]"
-        >
-          {children}
-        </main>
-        <MobileNav />
-      </div>
+      <TonProvider>
+        <EnergyTicker />
+        <div className="flex flex-col h-dvh bg-[#0c0c0f] overflow-hidden">
+          <GameHeader />
+          <EnergyStrip />
+          <main
+            className="flex-1 overflow-y-auto overscroll-contain
+                       [scrollbar-width:none] [-webkit-overflow-scrolling:touch]"
+          >
+            {children}
+          </main>
+          <MobileNav />
+        </div>
+      </TonProvider>
     </AuthProvider>
-  ) 
+  )
 }
