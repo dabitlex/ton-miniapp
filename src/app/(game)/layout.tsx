6575fc8ea45baf53bc1f3155ca9bfe932f582777
dabitlex <dabitlex@gmail.com>
2026-06-23 08:41:27 +0200

    Update layout.tsx

--- src/app/(game)/layout.tsx
+++ src/app/(game)/layout.tsx
@@ -9,28 +9,31 @@ import { EnergyTicker }     from '@/components/layout/EnergyTicker'
 import { MysteryBoxModal }  from '@/components/game/MysteryBoxModal'
 import { AchievementPopup } from '@/components/game/AchievementPopup'
 import { MaintenanceGate }  from '@/components/layout/MaintenanceGate'
+import { PlatformGate }     from '@/components/providers/PlatformGate'
 
 export default function GameLayout({ children }: { children: React.ReactNode }) {
   return (
     <AuthProvider>
       <QueryProvider>
         <TonProvider>
-          <MaintenanceGate>
-            <EnergyTicker />
-            {/* Animierter Aurora-Hintergrund hinter allem (fixed, z-0) */}
-            <AuroraBackground />
-            {/* Container transparent, damit der Hintergrund durchscheint */}
-            <div className="flex flex-col h-dvh overflow-hidden relative z-10">
-              <GameHeader />
-              <main className="flex-1 overflow-y-auto overscroll-contain relative z-10
-                               [scrollbar-width:none] [-webkit-overflow-scrolling:touch]">
-                {children}
-              </main>
-              <MobileNav />
-            </div>
-            <MysteryBoxModal />
-            <AchievementPopup />
-          </MaintenanceGate>
+          <PlatformGate>
+            <MaintenanceGate>
+              <EnergyTicker />
+              {/* Animierter Aurora-Hintergrund hinter allem (fixed, z-0) */}
+              <AuroraBackground />
+              {/* Container transparent, damit der Hintergrund durchscheint */}
+              <div className="flex flex-col h-dvh overflow-hidden relative z-10">
+                <GameHeader />
+                <main className="flex-1 overflow-y-auto overscroll-contain relative z-10
+                                 [scrollbar-width:none] [-webkit-overflow-scrolling:touch]">
+                  {children}
+                </main>
+                <MobileNav />
+              </div>
+              <MysteryBoxModal />
+              <AchievementPopup />
+            </MaintenanceGate>
+          </PlatformGate>
         </TonProvider>
       </QueryProvider>
     </AuthProvider>
